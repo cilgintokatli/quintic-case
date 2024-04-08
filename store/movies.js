db20934c5aca6ee5b69ac5f017a6c83e7ed869a0
Okan Sahin <okansahinfed@gmail.com>
2024-04-08 10:34:06 +0300

    fix: check if movie is already store then add each movie

--- store/movies.js
+++ store/movies.js
@@ -18,48 +18,106 @@ export const mutations = {
 };
 
 export const actions = {
-  async fetchInitialMovies({ commit }, { $config }) {
+  // async fetchInitialMovies({ commit }, { $config }) {
+  //   try {
+  //     this.$axios.setToken($config.oneApiSecret, "Bearer");
+
+  //     // one api movie endpointi için fetch isteği
+
+  //     const response = await this.$axios.get(`${$config.oneApiBaseURL}/movie/`);
+
+  //     // her movie elemanına film afişi eklemek için TMBD API'ya paralel async fetch isteği
+  //     const moviesWithImages = await Promise.all(
+  //       response.data.docs.map(async (movie) => {
+  //         const imageUrl = await this.$axios
+  //           .get(`https://api.themoviedb.org/3/search/movie`, {
+  //             params: {
+  //               query: movie.name,
+  //             },
+  //             headers: {
+  //               Authorization: `Bearer ${$config.tmdbApiAccessToken}`,
+  //             },
+  //           })
+  //           .then((response) => {
+  //             const posterPath = response.data.results[0]?.poster_path;
+  //             if (posterPath) {
+  //               return `https://image.tmdb.org/t/p/w300${posterPath}`;
+  //             } else {
+  //               return null;
+  //             }
+  //           })
+  //           .catch((error) => {
+  //             console.error(
+  //               "tmdb api film afişi fetchlerken bir hata oldu:",
+  //               error
+  //             );
+  //             return null;
+  //           });
+
+  //         return { ...movie, imageUrl };
+  //       })
+  //     );
+  //     commit("SET_MOVIES", moviesWithImages);
+  //   } catch (error) {
+  //     console.error("fetchlerken bir hata oldu:", error);
+  //   }
+  // },
+  async fetchInitialMovies({ commit, state }, { $config }) {
     try {
       this.$axios.setToken($config.oneApiSecret, "Bearer");
 
-      // one api movie endpointi için fetch isteği
-
+      // Fetch movies
       const response = await this.$axios.get(`${$config.oneApiBaseURL}/movie/`);
 
-      // her movie elemanına film afişi eklemek için TMBD API'ya paralel async fetch isteği
       const moviesWithImages = await Promise.all(
         response.data.docs.map(async (movie) => {
-          const imageUrl = await this.$axios
-            .get(`https://api.themoviedb.org/3/search/movie`, {
-              params: {
-                query: movie.name,
-              },
-              headers: {
-                Authorization: `Bearer ${$config.tmdbApiAccessToken}`,
-              },
-            })
-            .then((response) => {
-              const posterPath = response.data.results[0]?.poster_path;
-              if (posterPath) {
-                return `https://image.tmdb.org/t/p/w300${posterPath}`;
-              } else {
-                return null;
-              }
-            })
-            .catch((error) => {
-              console.error(
-                "tmdb api film afişi fetchlerken bir hata oldu:",
-                error
-              );
-              return null;
-            });
+          // film storeda zaten var mı kontrol
+          const existingMovie = state.movies.find((m) => m._id === movie._id);
 
-          return { ...movie, imageUrl };
+          if (!existingMovie) {
+            // eğer yeni film ise, TMBD API'ya paralel async fetch isteği
+            const imageUrl = await this.$axios
+              .get(`https://api.themoviedb.org/3/search/movie`, {
+                params: {
+                  query: movie.name,
+                },
+                headers: {
+                  Authorization: `Bearer ${$config.tmdbApiAccessToken}`,
+                },
+              })
+              .then((response) => {
+                const posterPath = response.data.results[0]?.poster_path;
+                if (posterPath) {
+                  return `https://image.tmdb.org/t/p/w300${posterPath}`;
+                } else {
+                  return null;
+                }
+              })
+              .catch((error) => {
+                console.error(
+                  "Error fetching movie poster from TMDB API:",
+                  error
+                );
+                return null;
+              });
+
+            return { ...movie, imageUrl };
+          } else {
+            // var olan filmi geri döndür
+            return existingMovie;
+          }
         })
       );
-      commit("SET_MOVIES", moviesWithImages);
+
+      // yeni filmi store'a ekle
+      const newMovies = moviesWithImages.filter(
+        (movie) => !state.movies.find((m) => m._id === movie._id)
+      );
+      if (newMovies.length > 0) {
+        commit("SET_MOVIES", [...state.movies, ...newMovies]);
+      }
     } catch (error) {
-      console.error("fetchlerken bir hata oldu:", error);
+      console.error("An error occurred during fetching:", error);
     }
   },
   async fetchSingleMovie({ commit }, { $config, movieID }) {
